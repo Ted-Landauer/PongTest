e6a550c36cdff73f447b767e0187e92a44fe12e5
Ted Landauer <tedlandauer@gmail.com>
2026-02-06 16:33:37 -0800

    added in scoring limit system, new game, and exit game functions

--- scripts/main.gd
+++ scripts/main.gd
@@ -14,9 +14,35 @@ func _on_score_left_body_entered(body: Node2D) -> void:
 	score[1] += 1
 	$HUD/CPUScore.text = str(score[1])
 	$BallTimer.start()
+	
+	if score[1] == 5:
+		$GameOver/WinnerMessage.text = "CPU Wins"
+		$GameOver.visible = true
+		print("Game Over: CPU Wins")
+		$BallTimer.stop()
 
 #Player Scoring
 func _on_score_right_body_entered(body: Node2D) -> void:
 	score[0] += 1
 	$HUD/PlayerScore.text = str(score[0])
 	$BallTimer.start()
+	
+	if score[0] == 5:
+		$GameOver/WinnerMessage.text = "Player Wins"
+		$GameOver.visible = true
+		print("Game Over: Player Wins")
+		$BallTimer.stop()
+
+
+func _on_new_game_button_pressed() -> void:
+	$GameOver.visible = false
+	score[0] = 0
+	$HUD/PlayerScore.text = str(score[0])
+	
+	score[1] = 0
+	$HUD/CPUScore.text = str(score[1])
+	$BallTimer.start()
+	
+
+func _on_exit_game_button_pressed() -> void:
+	get_tree().quit()
